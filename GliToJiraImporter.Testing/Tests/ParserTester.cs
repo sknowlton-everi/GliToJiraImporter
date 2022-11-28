fa38cb02aaee2ba40bc18e6d21552f4a2e051d57
sknowlton-everi <samantha.knowlton@everi.com>
2022-11-28 14:00:33 -0600

    Logs for testing

--- GliToJiraImporter.Testing/Tests/ParserTester.cs
+++ GliToJiraImporter.Testing/Tests/ParserTester.cs
@@ -397,8 +397,12 @@ namespace GliToJiraImporter.Testing.Tests
             var currentAssemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace(@"file:/", string.Empty);
             currentAssemblyPath = currentAssemblyPath.Replace(@"bin/Debug/net6.0", string.Empty);
             var relativePath = Path.Combine(currentAssemblyPath, folderName);
+            log.Debug("PATHS");
+            log.Debug(currentAssemblyPath);
+            log.Debug(relativePath);
+            log.Debug(currentAssemblyPath + folderName);
             //return Path.GetFullPath(relativePath);
-            return relativePath;
+            return currentAssemblyPath + folderName;
         }
     }
 }
